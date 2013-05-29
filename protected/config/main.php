ce1d4e7a385a16255f9a2dd475441ce97d683b99
tmushayahama <tmushayahama@gmail.com>
2013-05-29 19:24:28 -0400

    iitialized the project. Included all the components to get started. I will have to add more files to git ignore.

---
+++ protected/config/main.php
@@ -0,0 +1,133 @@
+<?php
+
+// uncomment the following to define a path alias
+// Yii::setPathOfAlias('local','path/to/local-folder');
+
+// This is the main Web application configuration. Any writable
+// CWebApplication properties can be configured here.
+return array(
+	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+	'name'=>'My Web Application',
+
+	// preloading 'log' component
+	'preload'=>array('log'),
+
+	// autoloading model and component classes
+	'import'=>array(
+		'application.models.*',
+		'application.components.*',
+                'application.modules.user.models.*',
+                'application.modules.user.components.*',
+                'application.modules.rights.*',
+                'application.modules.rights.components.*',
+	),
+
+	'modules'=>array(
+		// uncomment the following to enable the Gii tool
+		'user'=>array(
+                        'tableUsers' => 'tc_users',
+                        'tableProfiles' => 'tc_profiles',
+                        'tableProfileFields' => 'tc_profiles_fields',
+                        # encrypting method (php hash function)
+                        'hash' => 'md5',
+
+                        # send activation email
+                        'sendActivationMail' => true,
+
+                        # allow access for non-activated users
+                        'loginNotActiv' => false,
+
+                        # activate user on registration (only sendActivationMail = false)
+                        'activeAfterRegister' => false,
+
+                        # automatically login from registration
+                        'autoLogin' => true,
+
+                        # registration path
+                        'registrationUrl' => array('/user/registration'),
+
+                        # recovery password path
+                        'recoveryUrl' => array('/user/recovery'),
+
+                        # login form path
+                        'loginUrl' => array('/user/login'),
+
+                        # page after login
+                        'returnUrl' => array('/user/profile'),
+
+                        # page after logout
+                        'returnLogoutUrl' => array('/user/login'),
+                ),
+                'rights'=>array(
+                      'install'=>true,
+                ),
+		'gii'=>array(
+			'class'=>'system.gii.GiiModule',
+			'password'=>'awesome++',
+			// If removed, Gii defaults to localhost only. Edit carefully to taste.
+			'ipFilters'=>array('127.0.0.1','::1'),
+		),
+		
+	),
+
+	// application components
+	'components'=>array(
+		'user'=>array(
+			// enable cookie-based authentication
+			'class' => 'RWebUser',
+                        'allowAutoLogin'=>true,
+                        'loginUrl' => array('/user/login'),
+		),
+		// uncomment the following to enable URLs in path-format
+		'authManager'=>array(
+                        'class'=>'RDbAuthManager',
+                        'connectionID'=>'db',
+                ),
+		'urlManager'=>array(
+			'urlFormat'=>'path',
+			'rules'=>array(
+				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
+				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+			),
+                        //'showScriptName'=>false,
+		),
+		// uncomment the following to use a MySQL database
+		
+		'db'=>array(
+			'connectionString' => 'mysql:host=localhost;dbname=timecat_v4',
+			'emulatePrepare' => true,
+			'username' => 'timecat4',
+			'password' => 'awesome++',
+			'charset' => 'utf8',
+                        'tablePrefix' => 'tc_',
+		),
+		
+		'errorHandler'=>array(
+			// use 'site/error' action to display errors
+			'errorAction'=>'site/error',
+		),
+		'log'=>array(
+			'class'=>'CLogRouter',
+			'routes'=>array(
+				array(
+					'class'=>'CFileLogRoute',
+					'levels'=>'error, warning',
+				),
+				// uncomment the following to show log messages on web pages
+				/*
+				array(
+					'class'=>'CWebLogRoute',
+				),
+				*/
+			),
+		),
+	),
+
+	// application-level parameters that can be accessed
+	// using Yii::app()->params['paramName']
+	'params'=>array(
+		// this is used in contact page
+		'adminEmail'=>'webmaster@example.com',
+	),
+);
