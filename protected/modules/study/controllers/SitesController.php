015ca20de14689d09d229664ead546e8c70978bd
Aron Aziz <asaziz123@gmail.com>
2013-07-11 10:57:46 -0400

    Fixed sites nav bar

--- protected/modules/study/controllers/SitesController.php
+++ protected/modules/study/controllers/SitesController.php
@@ -1,194 +1,184 @@
 <?php
 
-class SitesController extends Controller
-{
-	/**
-	 * @return array action filters
-	 */
-	public function filters()
-	{
-		return array(
-			'accessControl', // perform access control for CRUD operations
-			'postOnly + delete', // we only allow deletion via POST request
-		);
-	}
+class SitesController extends Controller {
 
-	/**
-	 * Specifies the access control rules.
-	 * This method is used by the 'accessControl' filter.
-	 * @return array access control rules
-	 */
-	public function accessRules()
-	{
-		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
-			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update','dashboard'),
-				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
-			),
-			array('deny',  // deny all users
-				'users'=>array('*'),
-			),
-		);
-	}
+    /**
+     * @return array action filters
+     */
+    public function filters() {
+        return array(
+            'accessControl', // perform access control for CRUD operations
+            'postOnly + delete', // we only allow deletion via POST request
+        );
+    }
 
-	public function actionDashboard($studyid) {
-		$this->populateStudyNav($studyid);
-		$sitesCriteria = new CDbCriteria();
-		$sitesCriteria->alias = 't1';
-		$sitesCriteria->condition = "t1.study_id=" . $studyid;
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules() {
+        return array(
+            array('allow', // allow all users to perform 'index' and 'view' actions
+                'actions' => array('index', 'view'),
+                'users' => array('*'),
+            ),
+            array('allow', // allow authenticated user to perform 'create' and 'update' actions
+                'actions' => array('create', 'update', 'dashboard'),
+                'users' => array('@'),
+            ),
+            array('allow', // allow admin user to perform 'admin' and 'delete' actions
+                'actions' => array('admin', 'delete'),
+                'users' => array('admin'),
+            ),
+            array('deny', // deny all users
+                'users' => array('*'),
+            ),
+        );
+    }
 
-		$sitesModel = new Sites;
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
+    public function actionDashboard($studyid) {
+        $this->populateStudyNav($studyid);
+        $sitesCriteria = new CDbCriteria();
+        $sitesCriteria->alias = 't1';
+        $sitesCriteria->condition = "t1.study_id=" . $studyid;
 
-		if (isset($_POST['update'])) {
-			$sitesModel = $this->loadModel(2);
-		}
-						
-		if (isset($_POST['Sites'])) {
-			$sitesModel->attributes = $_POST['Sites'];
-			$sitesModel->study_id = $studyid;
-			if ($sitesModel->save()) {
-				//$this->redirect(array('view', 'id' => $model->id));
-			}
-		}
-		$this->render('dashboard', array(
-				'sites_model' => $sitesModel,
-				'study_sites' => Sites::Model()->findAll($sitesCriteria),
-				'sites_timezones' => $sitesModel->getTimezones(),
-		));
-	}
-	/**
-	 * Displays a particular model.
-	 * @param integer $id the ID of the model to be displayed
-	 */
-	public function actionView($id)
-	{
-		$this->render('view',array(
-			'model'=>$this->loadModel($id),
-		));
-	}
+        $sitesModel = new Sites;
+        // Uncomment the following line if AJAX validation is needed
+        // $this->performAjaxValidation($model);
 
-	/**
-	 * Creates a new model.
-	 * If creation is successful, the browser will be redirected to the 'view' page.
-	 */
-	public function actionCreate()
-	{
-		$model=new Sites;
+        if (isset($_POST['update'])) {
+            $sitesModel = $this->loadModel(2);
+        }
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
+        if (isset($_POST['Sites'])) {
+            $sitesModel->attributes = $_POST['Sites'];
+            $sitesModel->study_id = $studyid;
+            if ($sitesModel->save()) {
+                //$this->redirect(array('view', 'id' => $model->id));
+            }
+        }
+        $this->render('dashboard', array(
+            'sites_model' => $sitesModel,
+            'study_sites' => Sites::Model()->findAll($sitesCriteria),
+            'sites_timezones' => $sitesModel->getTimezones(),
+            'studyId' => $studyid,
+        ));
+    }
 
-		if(isset($_POST['Sites']))
-		{
-			$model->attributes=$_POST['Sites'];
-			if($model->save())
-				$this->redirect(array('view','id'=>$model->id));
-		}
+    /**
+     * Displays a particular model.
+     * @param integer $id the ID of the model to be displayed
+     */
+    public function actionView($id) {
+        $this->render('view', array(
+            'model' => $this->loadModel($id),
+        ));
+    }
 
-		$this->render('create',array(
-			'model'=>$model,
-		));
-	}
+    /**
+     * Creates a new model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     */
+    public function actionCreate() {
+        $model = new Sites;
 
-	/**
-	 * Updates a particular model.
-	 * If update is successful, the browser will be redirected to the 'view' page.
-	 * @param integer $id the ID of the model to be updated
-	 */
-	public function actionUpdate($id)
-	{
-		$model=$this->loadModel($id);
+        // Uncomment the following line if AJAX validation is needed
+        // $this->performAjaxValidation($model);
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
+        if (isset($_POST['Sites'])) {
+            $model->attributes = $_POST['Sites'];
+            if ($model->save())
+                $this->redirect(array('view', 'id' => $model->id));
+        }
 
-		if(isset($_POST['Sites']))
-		{
-			$model->attributes=$_POST['Sites'];
-			if($model->save())
-				$this->redirect(array('view','id'=>$model->id));
-		}
+        $this->render('create', array(
+            'model' => $model,
+        ));
+    }
 
-		$this->render('update',array(
-			'model'=>$model,
-		));
-	}
+    /**
+     * Updates a particular model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id the ID of the model to be updated
+     */
+    public function actionUpdate($id) {
+        $model = $this->loadModel($id);
 
-	/**
-	 * Deletes a particular model.
-	 * If deletion is successful, the browser will be redirected to the 'admin' page.
-	 * @param integer $id the ID of the model to be deleted
-	 */
-	public function actionDelete($id)
-	{
-		$this->loadModel($id)->delete();
+        // Uncomment the following line if AJAX validation is needed
+        // $this->performAjaxValidation($model);
 
-		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-		if(!isset($_GET['ajax']))
-			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
-	}
+        if (isset($_POST['Sites'])) {
+            $model->attributes = $_POST['Sites'];
+            if ($model->save())
+                $this->redirect(array('view', 'id' => $model->id));
+        }
 
-	/**
-	 * Lists all models.
-	 */
-	public function actionIndex()
-	{
-		$dataProvider=new CActiveDataProvider('Sites');
-		$this->render('index',array(
-			'dataProvider'=>$dataProvider,
-		));
-	}
+        $this->render('update', array(
+            'model' => $model,
+        ));
+    }
 
-	/**
-	 * Manages all models.
-	 */
-	public function actionAdmin()
-	{
-		$model=new Sites('search');
-		$model->unsetAttributes();  // clear any default values
-		if(isset($_GET['Sites']))
-			$model->attributes=$_GET['Sites'];
+    /**
+     * Deletes a particular model.
+     * If deletion is successful, the browser will be redirected to the 'admin' page.
+     * @param integer $id the ID of the model to be deleted
+     */
+    public function actionDelete($id) {
+        $this->loadModel($id)->delete();
 
-		$this->render('admin',array(
-			'model'=>$model,
-		));
-	}
+        // if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
+        if (!isset($_GET['ajax']))
+            $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+    }
 
-	/**
-	 * Returns the data model based on the primary key given in the GET variable.
-	 * If the data model is not found, an HTTP exception will be raised.
-	 * @param integer $id the ID of the model to be loaded
-	 * @return Sites the loaded model
-	 * @throws CHttpException
-	 */
-	public function loadModel($id)
-	{
-		$model=Sites::model()->findByPk($id);
-		if($model===null)
-			throw new CHttpException(404,'The requested page does not exist.');
-		return $model;
-	}
+    /**
+     * Lists all models.
+     */
+    public function actionIndex() {
+        $dataProvider = new CActiveDataProvider('Sites');
+        $this->render('index', array(
+            'dataProvider' => $dataProvider,
+        ));
+    }
 
-	/**
-	 * Performs the AJAX validation.
-	 * @param Sites $model the model to be validated
-	 */
-	protected function performAjaxValidation($model)
-	{
-		if(isset($_POST['ajax']) && $_POST['ajax']==='sites-form')
-		{
-			echo CActiveForm::validate($model);
-			Yii::app()->end();
-		}
-	}
+    /**
+     * Manages all models.
+     */
+    public function actionAdmin() {
+        $model = new Sites('search');
+        $model->unsetAttributes();  // clear any default values
+        if (isset($_GET['Sites']))
+            $model->attributes = $_GET['Sites'];
+
+        $this->render('admin', array(
+            'model' => $model,
+        ));
+    }
+
+    /**
+     * Returns the data model based on the primary key given in the GET variable.
+     * If the data model is not found, an HTTP exception will be raised.
+     * @param integer $id the ID of the model to be loaded
+     * @return Sites the loaded model
+     * @throws CHttpException
+     */
+    public function loadModel($id) {
+        $model = Sites::model()->findByPk($id);
+        if ($model === null)
+            throw new CHttpException(404, 'The requested page does not exist.');
+        return $model;
+    }
+
+    /**
+     * Performs the AJAX validation.
+     * @param Sites $model the model to be validated
+     */
+    protected function performAjaxValidation($model) {
+        if (isset($_POST['ajax']) && $_POST['ajax'] === 'sites-form') {
+            echo CActiveForm::validate($model);
+            Yii::app()->end();
+        }
+    }
+
 }
--- protected/modules/study/views/sites/dashboard.php
+++ protected/modules/study/views/sites/dashboard.php
@@ -1,113 +1,114 @@
 <?php $this->beginContent('//study_layouts/study_nav'); ?>
 <ul class="breadcrumbs">
-	<li><a href="onsestudy.html">Study Home</a></li>
-	<li class="current"><a href="sites.html">Sites</a></li>
+    <?php echo CHtml::link('Study Home', array('/study/study/dashboard/studyid/' . $studyId)); ?>
+<!--    <li><a href="onsestudy.html">Study Home</a></li>-->
+    <li class="current">Sites</li>
 </ul>	
 <div class="row">
-	<div class="large-7 columns">
-		<?php foreach ($study_sites as $site): ?>
-			<div class="row margibotom">
-				<div class="large-11 large-centered columns regordoon blanko sear">
-					<div class="row minpad">
-						<div class="large-12 columns stid" data-stid="1">
-							<span class="vnam"><?php echo $site->name ?></span>
-							<span class="round success label right">active</span>
-						</div>
-					</div>
-					<div class="row">
-						<div class="small-10 columns">
-							<div class="row minpad">
-								<div class="large-3 columns">
-									<strong>Time-Zone:</strong>
-								</div>
-								<div class="large-9 columns vdef" data-thetmz="America/Anchorage">
-									<?php echo $site->timezone; ?>
-								</div>
-							</div>
-							<div class="row minpad">
-								<div class="large-3 columns">
-									<strong>Current Time:</strong>
-								</div>
-								<div class="large-9 columns vstars">
-									18:25
-								</div>
-							</div>
-						</div>
-						<div class="small-2 columns">
-							<img src="<?php echo Yii::app()->request->baseUrl . '/img/site1.png'?>" alt="site icon" />
-						</div>
-					</div>
-					<div class="row celeste">
-						<div class="small-12 columns">
-							<a href="#" class="button small secondary nomarg editers">Edit</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		<?php endforeach ?>
-	</div>
-	<div class="large-5 columns">
-		<?php
-		echo $this->renderPartial('_form', array('sites_model' => $sites_model,
-				'sites_timezones' => $sites_timezones));
-		?>
-	</div>
+    <div class="large-7 columns">
+        <?php foreach ($study_sites as $site): ?>
+            <div class="row margibotom">
+                <div class="large-11 large-centered columns regordoon blanko sear">
+                    <div class="row minpad">
+                        <div class="large-12 columns stid" data-stid="1">
+                            <span class="vnam"><?php echo $site->name ?></span>
+                            <span class="round success label right">active</span>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="small-10 columns">
+                            <div class="row minpad">
+                                <div class="large-3 columns">
+                                    <strong>Time-Zone:</strong>
+                                </div>
+                                <div class="large-9 columns vdef" data-thetmz="America/Anchorage">
+                                    <?php echo $site->timezone; ?>
+                                </div>
+                            </div>
+                            <div class="row minpad">
+                                <div class="large-3 columns">
+                                    <strong>Current Time:</strong>
+                                </div>
+                                <div class="large-9 columns vstars">
+                                    18:25
+                                </div>
+                            </div>
+                        </div>
+                        <div class="small-2 columns">
+                            <img src="<?php echo Yii::app()->request->baseUrl . '/img/site1.png' ?>" alt="site icon" />
+                        </div>
+                    </div>
+                    <div class="row celeste">
+                        <div class="small-12 columns">
+                            <a href="#" class="button small secondary nomarg editers">Edit</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach ?>
+    </div>
+    <div class="large-5 columns">
+        <?php
+        echo $this->renderPartial('_form', array('sites_model' => $sites_model,
+            'sites_timezones' => $sites_timezones));
+        ?>
+    </div>
 </div>
 <div id="edit-sites-modal" class="reveal-modal medium">
-	<h2>Edit Site.</h2>
-	<?php
-	$form = $this->beginWidget('CActiveForm', array(
-			'id' => 'sites-form',
-			'enableAjaxValidation' => false,
-	));
-	?>
-	<div class="row">
-		<div class="large-9 large-centered columns celeste inputers">
-			<div class="row">
-				<div class="large-3 columns">
-					<?php echo $form->labelEx($sites_model, 'name'); ?>
-				</div>
-				<div class="large-9 columns">
-					<?php echo $form->textField($sites_model, 'name', array('id' => 'edit-sitesname-field', 'maxlength' => 50)); ?>
-					<?php echo $form->error($sites_model, 'name'); ?>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="row">
-		<div class="large-9 large-centered columns celeste inputers">
-			<div class="row">
-				<div class="large-3 columns">
-					<?php echo $form->labelEx($sites_model, 'timezone'); ?>
-				</div>
-				<div class="large-9 columns">
-					<?php echo $form->dropDownList($sites_model, 'timezone', CHtml::listData($sites_timezones, 'name', 'name')); //$sites_types); ?>
-					<?php echo $form->error($sites_model, 'timezone'); ?>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="row">
-		<div class="large-9 large-centered columns celeste inputers">
-			<div class="row">
-				<div class="large-3 columns">
-					<?php echo $form->labelEx($sites_model, 'description'); ?>
-				</div>
-				<div class="large-9 columns">
-					<?php echo $form->textArea($sites_model, 'description', array('id' => 'edit-sitesdescription-field', 'maxlength' => 255)); ?>
-					<?php echo $form->error($sites_model, 'description'); ?>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="row">
-		<div class="small-12 columns text-right">
-			<?php
-			echo CHtml::tag('button', array('name' => 'update', 'class' => 'small button radius entrar'), '<i class="foundicon-plus"></i> Save Changes');
-			?>
-		</div>
-	</div>
-	<a class="close-reveal-modal">&#215;</a>
-	<?php $this->endWidget(); ?>
+    <h2>Edit Site.</h2>
+    <?php
+    $form = $this->beginWidget('CActiveForm', array(
+        'id' => 'sites-form',
+        'enableAjaxValidation' => false,
+    ));
+    ?>
+    <div class="row">
+        <div class="large-9 large-centered columns celeste inputers">
+            <div class="row">
+                <div class="large-3 columns">
+                    <?php echo $form->labelEx($sites_model, 'name'); ?>
+                </div>
+                <div class="large-9 columns">
+                    <?php echo $form->textField($sites_model, 'name', array('id' => 'edit-sitesname-field', 'maxlength' => 50)); ?>
+                    <?php echo $form->error($sites_model, 'name'); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="large-9 large-centered columns celeste inputers">
+            <div class="row">
+                <div class="large-3 columns">
+                    <?php echo $form->labelEx($sites_model, 'timezone'); ?>
+                </div>
+                <div class="large-9 columns">
+                    <?php echo $form->dropDownList($sites_model, 'timezone', CHtml::listData($sites_timezones, 'name', 'name')); //$sites_types); ?>
+                    <?php echo $form->error($sites_model, 'timezone'); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="large-9 large-centered columns celeste inputers">
+            <div class="row">
+                <div class="large-3 columns">
+                    <?php echo $form->labelEx($sites_model, 'description'); ?>
+                </div>
+                <div class="large-9 columns">
+                    <?php echo $form->textArea($sites_model, 'description', array('id' => 'edit-sitesdescription-field', 'maxlength' => 255)); ?>
+                    <?php echo $form->error($sites_model, 'description'); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="small-12 columns text-right">
+            <?php
+            echo CHtml::tag('button', array('name' => 'update', 'class' => 'small button radius entrar'), '<i class="foundicon-plus"></i> Save Changes');
+            ?>
+        </div>
+    </div>
+    <a class="close-reveal-modal">&#215;</a>
+    <?php $this->endWidget(); ?>
 </div>
 <?php $this->endContent(); ?>
